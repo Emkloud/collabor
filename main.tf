f44b21ff86a944545f609c1fdc351d65c3054f8f
Emcloud <emma4real56@gmail.com>
2025-03-14 11:34:13 -0500

    resolving conflict

--- main.tf
+++ main.tf
@@ -12,22 +12,13 @@ resource "aws_instance" "ec2_instance" {
   # Additional instance configurations can be added here, such as security groups, subnet, etc.
   
   tags = {
-<<<<<<< HEAD
+
     Name = "lesson-${count.index + 1}"
   }
 }
 
 resource "aws_s3_bucket" "bisola_bucket" {
   bucket = "old-rugged-bucket"
-=======
-    Name = "master_node-${count.index + 3}"
-  }
-}
-
-resource "aws_s3_bucket" "funtab_bucket" {
-  bucket = "old_new_knowledge_bucket"
->>>>>>> 684ef3edce8cfad5895c4838147e5d46baa132d9
-
   tags = {
     Name        = "London_bucket"
     Environment = "Dev"
